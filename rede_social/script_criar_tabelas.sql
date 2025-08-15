325ca7f49d19cd941f6841c5a09b7cbb48bb64b8
Moisés <olimpiodev.tec@gmail.com>
2025-08-15 19:38:46 -0300

    Add files via upload

--- rede_social/script_criar_tabelas.sql
+++ rede_social/script_criar_tabelas.sql
@@ -1,16 +1,15 @@
-
 /*Criando a tabela de posts*/
 CREATE TABLE posts (
-    post_id INT PRIMARY KEY,
+    post_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description TEXT,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP DEFAULT now()
 );
 
 /*Criando a tabela de comentarios*/
 CREATE TABLE comments (
-    comment_id INT PRIMARY KEY,
+    comment_id SERIAL PRIMARY KEY,
     comment TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
     post_id INT REFERENCES posts(post_id)
 );
